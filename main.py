e34223f1603ae160af8bb8d01d9a535b0c8c2417
Larry <delvinlowzy@gmail.com>
2015-09-01 07:04:06 +0800

    Correct HTML errors, change Data Model from TriageReading to PatientProfile and added more fields.

--- main.py
+++ main.py
@@ -36,10 +36,20 @@ class HospitalStaff(ndb.Model):
     email = ndb.StringProperty(indexed=False)
 
 
-class TriageReading(ndb.Model):
+class PatientProfile(ndb.Model):
     """A main model for representing an individual Patient entry."""
     hospitalStaff = ndb.StructuredProperty(HospitalStaff)
-    nricNum = ndb.StringProperty()
+    # Profile information
+    name = ndb.StringProperty()
+    nric_num = ndb.StringProperty()
+    gender = ndb.StringProperty(indexed=False)
+    nationality = ndb.StringProperty(indexed=False)
+    dob = ndb.StringProperty(indexed=False)
+    race = ndb.StringProperty(indexed=False)
+    mobile_number = ndb.StringProperty(indexed=False)
+    address = ndb.StringProperty(indexed=False)
+    add_info = ndb.StringProperty(indexed=False)
+    # Triage Readings
     temperature = ndb.StringProperty(indexed=False)
     heartrate = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
@@ -55,8 +65,8 @@ class ListAll(webapp2.RequestHandler):
     def get(self):
         hospital_name = self.request.get('hospital_name',
                                          DEFAULT_HOSPITAL_NAME)
-        readings_query = TriageReading.query(
-            ancestor=hospital_key(hospital_name)).order(-TriageReading.date)
+        readings_query = PatientProfile.query(
+            ancestor=hospital_key(hospital_name)).order(-PatientProfile.date)
         readings = readings_query.fetch(10)
 
         user = users.get_current_user()
@@ -78,10 +88,9 @@ class ListAll(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('listall_san.html')
         self.response.write(template.render(template_values))
 
-
 class Create(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('create.html')
+        template = JINJA_ENVIRONMENT.get_template('registration.html')
         self.response.write(template.render())
 
     def post(self):
@@ -90,23 +99,32 @@ class Create(webapp2.RequestHandler):
         # single entity group will be consistent. However, the write
         # rate to a single entity group should be limited to
         # ~1/second.
-        reading = TriageReading(parent=hospital_key(DEFAULT_HOSPITAL_NAME))
+        reading = PatientProfile(parent=hospital_key(DEFAULT_HOSPITAL_NAME))
 
         if users.get_current_user():
             reading.hospitalStaff = HospitalStaff(
                 identity=users.get_current_user().user_id(),
                 email=users.get_current_user().email())
 
-        nric_of_patient = self.request.get('nricNum')
-        temp_of_patient = self.request.get('temperature')
-        heartrate_of_patient = self.request.get('heartbeat')
+        patient_name = self.request.get('name')
+        patient_nric = self.request.get('nricNum')
+        patient_gender = self.request.get('gender')
+        patient_dob = self.request.get('dob')
+        patient_race = self.request.get('race')
+        patient_mobile_num = self.request.get('mobile_number')
+        patient_address = self.request.get('address')
+        patient_add_info = self.request.get('add_info')
 
         # Do some input validation before putting data into Datastore
-        if (nric_of_patient.isalnum() and len(nric_of_patient) == 9
-            and temp_of_patient.isdigit() and heartrate_of_patient.isdigit()):
-            reading.nricNum = nric_of_patient
-            reading.temperature = heartrate_of_patient
-            reading.heartrate = heartrate_of_patient
+        if patient_nric.isalnum() and len(patient_nric) == 9 :
+            reading.name = patient_name
+            reading.nric_num = patient_nric
+            reading.gender = patient_gender
+            reading.dob = patient_dob
+            reading.race = patient_race
+            reading.mobile_number = patient_mobile_num
+            reading.address = patient_address
+            reading.add_info = patient_add_info
             reading.put()
 
         query_params = {'hospital_name': DEFAULT_HOSPITAL_NAME}
@@ -119,12 +137,10 @@ class Triage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('triage.html')
         self.response.write(template.render())
 
-
 class Faq(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('faq.html')
         self.response.write(template.render())
-
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
